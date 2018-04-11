1827e44e628e51fb44987e0c3532211d393209ed
Hannes Bartle <hannes@michaelbartle.de>
2018-04-11 21:14:02 +0200

    Refactor Code

--- cv_challenge.m
+++ cv_challenge.m
@@ -22,166 +22,169 @@ load('data/test/testVectors.mat')
 
 [D,N] = size(trainVectors);
 [D,M] = size(valVectors);
+[D,L] = size(testVectors);
 classes = unique(trainLbls);
 K = length(classes);
 
-%% Nearest Centroid 
-disp('***********************************')
-disp('Nearest Centroid Classification')
-[predLbls, mean_vectors] = ncClassifier(trainVectors,valVectors,trainLbls);
-nc_score = length(find(predLbls-valLbls'==0))/length(valLbls);
-disp(['[*] Max Score: ',num2str(nc_score*100),'%'])
+trainVectorsStd = (trainVectors - repmat(mean(trainVectors,2),1,N))./repmat(var(trainVectors),D,1);
+valVectorsStd = (valVectors - repmat(mean(valVectors,2),1,M))./repmat(var(valVectors),D,1);
+testVectorsStd = (testVectors - repmat(mean(testVectors,2),1,L))./repmat(var(testVectors),D,1);
 
-%% Nearest Subclass Centroid 
-disp('***********************************')
-disp('Nearest Subclass Centroid Classification')
-disp('[*] Optimizing over hyperparameter space...')
-K_sc = 5;
-for k=1:K_sc
-    predLbls = nscClassifier(trainVectors,valVectors,trainLbls,k);
-    nsc_sc(k) = length(find(predLbls-valLbls'==0))/length(valLbls);
-    disp(['[*]' num2str(k) ' of ' num2str(K_sc)])
-end
-[nsc_score, Kmax] = max(nsc_sc);
-disp(['[*] K: ',num2str(Kmax)])
-disp(['[*] Max Score: ',num2str(nsc_score*100),'%'])
-
-%% Nearest Neighbor 
-disp('***********************************')
-disp('Nearest Centroid Classification')
-predLbls = nnClassifier(trainVectors,valVectors,trainLbls);
-nn_score = length(find(predLbls-valLbls'==0))/length(valLbls);
-disp(['[*] Max Score: ',num2str(nn_score*100),'%'])
-
-
-%% Linear Regression 
-
-% Label Matrix
-T = zeros(K,N);
-for n=1:K
-    T(n,trainLbls==n) = 1;
-end
-
-
-% Regularization Parameter Set
-Cvec = 10.^(-4:3);
-
-
-% Initialize Matrices
-pred_test_lbls = cell(length(Cvec));
-LMS_CR = zeros(length(Cvec));
-
-% Precompute Training Vector Covariance
-X=trainVectors*trainVectors';
-disp('***********************************')
-disp('LMS Regression-based Classification')
-disp('[*] Optimizing over hyperparameter space...')
-for cc=1:length(Cvec)
-    
-    C = Cvec(cc);
-
-    % Get the Weights
-    W = (X+ C*eye(D))\trainVectors * T';
-    
-    
-    % Classify Validation Samples
-    OutputVal = W'*valVectors;
-    [maxOt,pred_val_Lbls] = max(OutputVal);
-    
-    % Classify Testing Set
-    OutputTest = W'*testVectors;
-    [~,pred_test_lbls{cc}] = max(OutputTest);
-    
-    
-    % Measure Performance
-    LMS_CR(cc) = length(find(pred_val_Lbls-valLbls'==0)) / length(valLbls);
-    disp(['[*] ' num2str(cc) ' of ' num2str(length(Cvec))])
-end
-[score,ind] = max(LMS_CR(:));
-disp(['C-Value: ',num2str(Cvec(ind))])
-disp(['Max Score: ',num2str(score*100),'%'])
-
-index = 1:size(testVectors,2);
-file = fopen('submissions/testLbls_LMS.txt','w');
-fprintf(file,'%s,%s\n','ID','Label');
-fprintf(file,'%d,%d\n',[index; pred_test_lbls{ind}]);
-fclose(file);
+% %% Nearest Centroid 
+% disp('***********************************')
+% disp('Nearest Centroid Classification')
+% [predLbls, mean_vectors] = ncClassifier(trainVectors,valVectors,trainLbls);
+% nc_score = length(find(predLbls-valLbls'==0))/length(valLbls);
+% disp(['[*] Max Score: ',num2str(nc_score*100),'%'])
+% 
+% %% Nearest Subclass Centroid 
+% disp('***********************************')
+% disp('Nearest Subclass Centroid Classification')
+% disp('[*] Optimizing over hyperparameter space...')
+% K_sc = 5;
+% for k=1:K_sc
+%     predLbls = nscClassifier(trainVectors,valVectors,trainLbls,k);
+%     nsc_sc(k) = length(find(predLbls-valLbls'==0))/length(valLbls);
+%     disp(['[*]' num2str(k) ' of ' num2str(K_sc)])
+% end
+% [nsc_score, Kmax] = max(nsc_sc);
+% disp(['[*] K: ',num2str(Kmax)])
+% disp(['[*] Max Score: ',num2str(nsc_score*100),'%'])
+% 
+% %% Nearest Neighbor 
+% disp('***********************************')
+% disp('Nearest Centroid Classification')
+% predLbls = nnClassifier(trainVectors,valVectors,trainLbls);
+% nn_score = length(find(predLbls-valLbls'==0))/length(valLbls);
+% disp(['[*] Max Score: ',num2str(nn_score*100),'%'])
+% 
+% 
+% %% Linear Regression 
+% 
+% % Label Matrix
+% T = zeros(K,N);
+% for n=1:K
+%     T(n,trainLbls==n) = 1;
+% end
+% 
+% 
+% % Regularization Parameter Set
+% Cvec = 10.^(-4:3);
+% 
+% 
+% % Initialize Matrices
+% pred_test_lbls = cell(length(Cvec));
+% LMS_CR = zeros(length(Cvec));
+% 
+% % Precompute Training Vector Covariance
+% X=trainVectors*trainVectors';
+% disp('***********************************')
+% disp('LMS Regression-based Classification')
+% disp('[*] Optimizing over hyperparameter space...')
+% for cc=1:length(Cvec)
+%     
+%     C = Cvec(cc);
+% 
+%     % Get the Weights
+%     W = (X+ C*eye(D))\trainVectors * T';
+%     
+%     
+%     % Classify Validation Samples
+%     OutputVal = W'*valVectors;
+%     [maxOt,pred_val_Lbls] = max(OutputVal);
+%     
+%     % Classify Testing Set
+%     OutputTest = W'*testVectors;
+%     [~,pred_test_lbls{cc}] = max(OutputTest);
+%     
+%     
+%     % Measure Performance
+%     LMS_CR(cc) = length(find(pred_val_Lbls-valLbls'==0)) / length(valLbls);
+%     disp(['[*] ' num2str(cc) ' of ' num2str(length(Cvec))])
+% end
+% [score,ind] = max(LMS_CR(:));
+% disp(['C-Value: ',num2str(Cvec(ind))])
+% disp(['Max Score: ',num2str(score*100),'%'])
+% 
+% index = 1:size(testVectors,2);
+% file = fopen('submissions/testLbls_LMS.txt','w');
+% fprintf(file,'%s,%s\n','ID','Label');
+% fprintf(file,'%d,%d\n',[index; pred_test_lbls{ind}]);
+% fclose(file);
 
 %% Kernel-based Regression
 
-% Label Matrix
-T = zeros(K,N);
-for n=1:K
-    T(n,trainLbls==n) = 1;
-end
+% rbfRegression([trainVectors valVectors(:,1:1500)],[trainLbls; valLbls(1:1500)],valVectors,valLbls,testVectors,K,N+15qqßq00)
 
-% Hyperparameter  Set
-Cvec = 0.18:0.01:0.2;
-Svec = 0.5:0.1:0.6;
+%% MLP
+% % Label Matrix
+% T = zeros(K,N);
+% for n=1:K
+%     T(n,trainLbls==n) = 1;
+% end
+% 
+% net =feedforwardnet([100 100],'trainscg');
+% %net.layers{3}.transferFcn = 'softmax';
+% 
+% [net,tr] = train(net,trainVectors,T);
+% 
+% predLbls = vec2ind(net(valVectors));
+% score = length(find(predLbls-valLbls'==0)) / length(valLbls);
 
-
-
-disp('***********************************')
-disp('RBF-Kernel-Regression-based Classification')
-
-% Precompute Training Set Covariance
-G_train = distance_matrix(trainVectors,trainVectors);
-sigma_train = mean(mean(G_train));
-% Precompute Validation Set Covariance
-Gval = distance_matrix(valVectors,trainVectors);
-sigma_val = mean(mean(Gval));
-% Precompute Testing Set Covariance
-Gtest = distance_matrix(testVectors,trainVectors);
-sigma_test = mean(mean(Gtest));
-
-
-
-pred_test_lbls = cell(length(Svec),length(Cvec));
-KR_CR = zeros(length(Svec),length(Cvec));
-disp('[*] Optimizing over hyperparameter space...')
-for ss=1:length(Svec)
-    for cc=1:length(Cvec)
-        
-        S = Svec(ss);
-        C = Cvec(cc);
-        
-        % Kernel Matrix of Training Set
-        Ktrain = exp(-G_train/(S*sigma_train));
-        % Get Weight Matrix
-        A = (Ktrain + C*eye(N))\ T';
-        
-        % Kernel Matrix of Validation Set
-        Kval = exp(-Gval/(S*sigma_val));
-        % Classify Validation Set
-        OutputVal = A'*Kval;
-        [~,pred_val_lbls] = max(OutputVal);
-        
-        
-        % Kernel Matrix of Testing Set
-        Ktest = exp(-Gtest/(S*sigma_test));
-        % Classify Validation Set
-        OutputTest = A'*Ktest;
-        [~,pred_test_lbls{ss,cc}] = max(OutputTest);
-        
-        
-        % Measure Performance
-        KR_CR(ss,cc) = length(find(pred_val_lbls-valLbls'==0)) / length(valLbls);
-        disp(['S: ' num2str(ss) ' of ' num2str(length(Svec));
-              'C: ' num2str(cc) ' of ' num2str(length(Cvec))])
-    end
-end
-[score,ind] = max(KR_CR(:));
-[Sidx,Cidx] = ind2sub(size(KR_CR),ind);
-disp(['C-Value: ',num2str(Cvec(Cidx)),', S-Value: ',num2str(Svec(Sidx))])
-disp(['Max Score: ',num2str(score*100),'%'])
-
-index = 1:size(testVectors,2);
-file = fopen('submissions/testLbls_KR.txt','w');
-fprintf(file,'%s,%s\n','ID','Label');
-fprintf(file,'%d,%d\n',[index; pred_test_lbls{Sidx,Cidx}]);
-fclose(file);
+%% Transferred-Learning with AlexNet
+% 
+% % Get Image Datastores
+% imdsTrain = imageDatastore('data/train/TrainImages');
+% imdsTrain.Labels = categorical(trainLbls);
+% 
+% imdsVal = imageDatastore('data/validation/ValidationImages');
+% imdsVal.Labels = categorical(valLbls);
+% 
+% imdsTest = imageDatastore('data/test/TestImages');
+% 
+% 
+% 
+% net = alexnet;
+% layersTransfer = net.Layers(1:end-8);
+% layers = [  layersTransfer
+%             fullyConnectedLayer(4096,'WeightLearnRateFactor',20,'BiasLearnRateFactor',20)
+%             reluLayer
+%             dropoutLayer
+%             fullyConnectedLayer(4096,'WeightLearnRateFactor',20,'BiasLearnRateFactor',20)
+%             reluLayer
+%             dropoutLayer
+%             fullyConnectedLayer(K,'WeightLearnRateFactor',20,'BiasLearnRateFactor',20)
+%             softmaxLayer
+%             classificationLayer];
+%         
+% inputSize = net.Layers(1).InputSize;
+% 
+% 
+% pixelRange = [-21 21];
+% imageAugmenter = imageDataAugmenter( ...
+%     'RandXReflection',true, ...
+%     'RandXTranslation',pixelRange, ...
+%     'RandYTranslation',pixelRange);
+% augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
+%     'DataAugmentation',imageAugmenter);
+% 
+% 
+% augimdsVal = augmentedImageDatastore(inputSize(1:2),imdsVal);
+% 
+% options = trainingOptions('sgdm', ...
+%     'MiniBatchSize',50, ...
+%     'MaxEpochs',6, ...
+%     'InitialLearnRate',1e-2, ...
+%     'ValidationData',augimdsVal, ...
+%     'ValidationFrequency',3, ...
+%     'ValidationPatience',Inf, ...
+%     'Verbose',false, ...
+%     'Plots','training-progress');
+% 
+% netTransfer = trainNetwork(augimdsTrain,layers,options);
+% 
 
 
-%% 
+
 
 
---
+++ functions/rbfRegression.m
@@ -0,0 +1,74 @@
+function [score] = rbfRegression(trainVectors,trainLbls,valVectors,valLbls,testVectors,K,N)
+% Label Matrix
+T = zeros(K,N);
+for n=1:K
+    T(n,trainLbls==n) = 1;
+end
+
+% Hyperparameter  Set
+Cvec = 0.18:0.01:0.2;
+Svec = 0.5:0.1:0.6;
+
+
+
+disp('***********************************')
+disp('RBF-Kernel-Regression-based Classification')
+
+% Precompute Training Set Covariance
+G_train = distance_matrix(trainVectors,trainVectors);
+sigma_train = mean(mean(G_train));
+% Precompute Validation Set Covariance
+Gval = distance_matrix(valVectors,trainVectors);
+sigma_val = mean(mean(Gval));
+% Precompute Testing Set Covariance
+Gtest = distance_matrix(testVectors,trainVectors);
+sigma_test = mean(mean(Gtest));
+
+
+
+pred_test_lbls = cell(length(Svec),length(Cvec));
+KR_CR = zeros(length(Svec),length(Cvec));
+disp('[*] Optimizing over hyperparameter space...')
+for ss=1:length(Svec)
+    for cc=1:length(Cvec)
+        
+        S = Svec(ss);
+        C = Cvec(cc);
+        
+        % Kernel Matrix of Training Set
+        Ktrain = exp(-G_train/(S*sigma_train));
+        % Get Weight Matrix
+        A = (Ktrain + C*eye(N))\ T';
+        
+        % Kernel Matrix of Validation Set
+        Kval = exp(-Gval/(S*sigma_val));
+        % Classify Validation Set
+        OutputVal = A'*Kval;
+        [~,pred_val_lbls] = max(OutputVal);
+        
+        
+        % Kernel Matrix of Testing Set
+        Ktest = exp(-Gtest/(S*sigma_test));
+        % Classify Validation Set
+        OutputTest = A'*Ktest;
+        [~,pred_test_lbls{ss,cc}] = max(OutputTest);
+        
+        
+        % Measure Performance
+        KR_CR(ss,cc) = length(find(pred_val_lbls-valLbls'==0)) / length(valLbls);
+        disp(['S: ' num2str(ss) ' of ' num2str(length(Svec));
+              'C: ' num2str(cc) ' of ' num2str(length(Cvec))])
+    end
+end
+[score,ind] = max(KR_CR(:));
+[Sidx,Cidx] = ind2sub(size(KR_CR),ind);
+disp(['C-Value: ',num2str(Cvec(Cidx)),', S-Value: ',num2str(Svec(Sidx))])
+disp(['Max Score: ',num2str(score*100),'%'])
+
+index = 1:size(testVectors,2);
+file = fopen('submissions/testLbls_KR.txt','w');
+fprintf(file,'%s,%s\n','ID','Label');
+fprintf(file,'%d,%d\n',[index; pred_test_lbls{Sidx,Cidx}]);
+fclose(file);
+end
+
